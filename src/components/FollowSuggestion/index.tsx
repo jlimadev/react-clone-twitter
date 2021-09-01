6a9e8558dde2545800c250ecc48bf84b3add800d
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-09-01 13:01:34 -0300

    feature(SideBar): create FollowSuggestion component

---
+++ src/components/FollowSuggestion/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Container, Avatar, Info, FollowButton } from './styles';
+
+interface FollowSuggestionProps {
+  name: string;
+  nickname: string;
+}
+
+const FollowSuggestion: React.FC<FollowSuggestionProps> = ({
+  name,
+  nickname,
+}) => {
+  return (
+    <Container>
+      <div>
+        <Avatar />
+
+        <Info>
+          <strong>{name}</strong>
+          <span>{nickname}</span>
+        </Info>
+      </div>
+
+      <FollowButton outlined>Follow</FollowButton>
+    </Container>
+  );
+};
+
+export default FollowSuggestion;
